1c90009f32209f23c01c212f732efee4d567bd2b
YuvaprakashN <73812241+YuvaprakashN@users.noreply.github.com>
2021-10-18 19:07:09 +0530

    Logout

--- controller/authContoller.js
+++ controller/authContoller.js
@@ -119,15 +119,15 @@ exports.resetPassword = catchAsyncErrors(async(req, res, next) => {
     sendToken(user, 200, res);
 });
 
-// // Logout user   =>   /api/v1/logout
-// exports.logout = catchAsyncErrors( async(req, res, next) => {
-//     res.cookie('token', 'none', {
-//         expires : new Date(Date.now()),
-//         httpOnly : true 
-//     });
+// Logout user   =>   /api/v1/logout
+exports.logout = catchAsyncErrors( async(req, res, next) => {
+    res.cookie('token', 'none', {
+        expires : new Date(Date.now()),
+        httpOnly : true 
+    });
 
-//     res.status(200).json({
-//         success : true,
-//         message : 'Logged out successfully.'
-//     });
-// });
+    res.status(200).json({
+        success : true,
+        message : 'Logged out successfully.'
+    });
+});
