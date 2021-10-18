b61aac22da2bf73aee06ae28435ebb9d339d8dba
YuvaprakashN <73812241+YuvaprakashN@users.noreply.github.com>
2021-10-18 16:55:01 +0530

    Protect route from unAuthorized user

--- routes/jobs.js
+++ routes/jobs.js
@@ -1,15 +1,16 @@
 const express=require("express")
 
 const router=express.Router()
-const {getJobs,getJob,jobStats, newjob,deleteJob,getJobsInRadius,updateJob}=require("../controller/jobsController")
+const {getJobs,getJob,jobStats, newjob,deleteJob,getJobsInRadius,updateJob}=require("../controller/jobsController");
+const { isAuthenticatedUser } = require("../middlewares/auth");
 //router.get("/jobs",getJobs)
-router.route("/jobs").get(getJobs)
+router.route("/jobs").get(isAuthenticatedUser,getJobs)
 router.route('/job/:id/:slug').get(getJob);
 
-router.route("/jobs").post(newjob)
+router.route("/jobs").post(isAuthenticatedUser,newjob)
 router.route('/jobs/:zipcode/:distance').get(getJobsInRadius);
-router.route('/job/:id').put( updateJob)
-    .delete(deleteJob);
+router.route('/job/:id').put(isAuthenticatedUser ,updateJob)
+    .delete(isAuthenticatedUser,deleteJob);
     router.route('/stats/:topic').get(jobStats);
 
 
