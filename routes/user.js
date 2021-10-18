d00896ebbee06b8ffdcc936a7aa5d98960647c06
YuvaprakashN <73812241+YuvaprakashN@users.noreply.github.com>
2021-10-18 20:54:15 +0530

    update user data and password

--- routes/user.js
+++ routes/user.js
@@ -3,8 +3,8 @@ const router = express.Router();
 
 const { 
     getUserProfile,
-    // updatePassword,
-    // updateUser,
+    updatePassword,
+     updateUser,
     // deleteUser,
     // getAppliedJobs,
     // getPublishedJobs,
@@ -17,11 +17,11 @@ const { isAuthenticatedUser, authorizeRoles } = require('../middlewares/auth');
 router.use(isAuthenticatedUser);
 
 router.route('/me').get(getUserProfile);
-// router.route('/jobs/applied').get(authorizeRoles('user'), getAppliedJobs)
+//router.route('/jobs/applied').get(authorizeRoles('user'), getAppliedJobs)
 // router.route('/jobs/published').get(authorizeRoles('employeer', 'admin'), getPublishedJobs)
 
-// router.route('/password/update').put(updatePassword);
-// router.route('/me/update').put(updateUser);
+router.route('/password/update').put(updatePassword);
+ router.route('/me/update').put(updateUser);
 
 // router.route('/me/delete').delete(deleteUser);
 
