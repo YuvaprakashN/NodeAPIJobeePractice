271a41bc756d476e1db28bee8cf57dc671bd2ccf
YuvaprakashN <73812241+YuvaprakashN@users.noreply.github.com>
2021-10-18 18:52:27 +0530

    Rest Password

--- controller/authContoller.js
+++ controller/authContoller.js
@@ -1,7 +1,6 @@
 const User = require('../models/users');
 const catchAsyncErrors = require('../middlewares/catchAsyncErrors');
 const ErrorHandler = require('../utils/errorHandler');
-//const sendToken = require('../utils/jwtToken');
 //const sendEmail = require('../utils/sendEmail');
 const crypto = require('crypto');
 const sendToken = require('../utils/jwtToken');
@@ -52,73 +51,73 @@ exports.loginUser = catchAsyncErrors( async (req, res, next) => {
 });
 
 // // Forgot Password  =>  /api/v1/password/forgot
-// exports.forgotPassword = catchAsyncErrors( async (req, res, next) => {
-//     const user = await User.findOne({email : req.body.email});
+exports.forgotPassword = catchAsyncErrors( async (req, res, next) => {
+    const user = await User.findOne({email : req.body.email});
 
-//     // Check user email is database
-//     if(!user) {
-//         return next(new ErrorHandler('No user found with this email.', 404));
-//     }
+    // Check user email is database
+    if(!user) {
+        return next(new ErrorHandler('No user found with this email.', 404));
+    }
 
-//     // Get reset token
-//     const resetToken = user.getResetPasswordToken();
+    // Get reset token
+    const resetToken = user.getResetPasswordToken();
 
-//     await user.save({ validateBeforeSave : false });
+    await user.save({ validateBeforeSave : false });
 
-//     // Create reset password url
-//     const resetUrl = `${req.protocol}://${req.get('host')}/api/v1/password/reset/${resetToken}`;
+    // Create reset password url
+    const resetUrl = `${req.protocol}://${req.get('host')}/api/v1/password/reset/${resetToken}`;
 
-//     const message = `Your password reset link is as follow:\n\n${resetUrl}\n\n If you have not request this, then please ignore that.`
+    const message = `Your password reset link is as follow:\n\n${resetUrl}\n\n If you have not request this, then please ignore that.`
 
-//     try {
-//         await sendEmail({
-//             email : user.email,
-//             subject : 'Jobbee-API Password Recovery',
-//             message
-//         });
+    try {
+        await sendEmail({
+            email : user.email,
+            subject : 'Jobbee-API Password Recovery',
+            message
+        });
     
-//         res.status(200).json({
-//             success : true,
-//             message : `Email sent successfully to: ${user.email}`
-//         });
-//     } catch (error) {
-//         user.resetPasswordToken = undefined;
-//         user.resetPasswordExpire = undefined;
+        res.status(200).json({
+            success : true,
+            message : `Email sent successfully to: ${user.email}`
+        });
+    } catch (error) {
+        user.resetPasswordToken = undefined;
+        user.resetPasswordExpire = undefined;
         
-//         await user.save({ validateBeforeSave : false });
+        await user.save({ validateBeforeSave : false });
 
-//         return next(new ErrorHandler('Email is not sent.'), 500);
-//     } 
+        return next(new ErrorHandler('Email is not sent.'), 500);
+    } 
 
-// });
+});
 
-// // Reset Password   =>   /api/v1/password/reset/:token
-// exports.resetPassword = catchAsyncErrors(async(req, res, next) => {
-//     // Hash url token
-//     const resetPasswordToken = crypto
-//         .createHash('sha256')
-//         .update(req.params.token)
-//         .digest('hex');
+// Reset Password   =>   /api/v1/password/reset/:token
+exports.resetPassword = catchAsyncErrors(async(req, res, next) => {
+    // Hash url token
+    const resetPasswordToken = crypto
+        .createHash('sha256')
+        .update(req.params.token)
+        .digest('hex');
 
-//     const user = await User.findOne({ 
-//         resetPasswordToken, 
-//         resetPasswordExpire: {$gt : Date.now() }
-//     });
+    const user = await User.findOne({ 
+        resetPasswordToken, 
+        resetPasswordExpire: {$gt : Date.now() }
+    });
 
-//     if(!user) {
-//         return next(new ErrorHandler('Password Reset token is invalid or has been expired.', 400));
-//     }
+    if(!user) {
+        return next(new ErrorHandler('Password Reset token is invalid or has been expired.', 400));
+    }
 
-//     // Setup new password
-//     user.password = req.body.password;
+    // Setup new password
+    user.password = req.body.password;
 
-//     user.resetPasswordToken = undefined;
-//     user.resetPasswordExpire = undefined;
+    user.resetPasswordToken = undefined;
+    user.resetPasswordExpire = undefined;
 
-//     await user.save();
+    await user.save();
 
-//     sendToken(user, 200, res);
-// });
+    sendToken(user, 200, res);
+});
 
 // // Logout user   =>   /api/v1/logout
 // exports.logout = catchAsyncErrors( async(req, res, next) => {
