52dcbc8220ff4dea7822851d48a3eb282319138b
YuvaprakashN <73812241+YuvaprakashN@users.noreply.github.com>
2021-10-18 18:53:48 +0530

    Reset Password

--- models/users.js
+++ models/users.js
@@ -66,21 +66,21 @@ userSchema.methods.comparePassword = async function(enterPassword) {
 }
 
 // Generate Password Reset Token
-// userSchema.methods.getResetPasswordToken = function() {
-//     // Generate token
-//     const resetToken = crypto.randomBytes(20).toString('hex');
+userSchema.methods.getResetPasswordToken = function() {
+    // Generate token
+    const resetToken = crypto.randomBytes(20).toString('hex');
 
-//     // Hash and set to resetPasswordToken
-//     this.resetPasswordToken = crypto
-//             .createHash('sha256')
-//             .update(resetToken)
-//             .digest('hex');
+    // Hash and set to resetPasswordToken
+    this.resetPasswordToken = crypto
+            .createHash('sha256')
+            .update(resetToken)
+            .digest('hex');
 
-//     // Set token expire time
-//     this.resetPasswordExpire = Date.now() + 30*60*1000;
+    // Set token expire time
+    this.resetPasswordExpire = Date.now() + 30*60*1000;
 
-//     return resetToken;
-// }
+    return resetToken;
+}
 
 // Show all jobs create by user using virtuals
 // userSchema.virtual('jobsPublished', {
