b68ee8a57f49f10cb1b0a5121403992e3c4bcb75
YuvaprakashN <73812241+YuvaprakashN@users.noreply.github.com>
2021-10-19 11:05:02 +0530

    apply job using resume

--- routes/jobs.js
+++ routes/jobs.js
@@ -1,13 +1,14 @@
 const express=require("express")
 
 const router=express.Router()
-const {getJobs,getJob,jobStats, newjob,deleteJob,getJobsInRadius,updateJob}=require("../controller/jobsController");
-const { isAuthenticatedUser } = require("../middlewares/auth");
+const {applyJob,getJobs,getJob,jobStats, newjob,deleteJob,getJobsInRadius,updateJob}=require("../controller/jobsController");
+const { isAuthenticatedUser, authorizeRoles } = require("../middlewares/auth");
 //router.get("/jobs",getJobs)
-router.route("/jobs").get(isAuthenticatedUser,getJobs)
+router.route("/jobs").get(getJobs)
 router.route('/job/:id/:slug').get(getJob);
+router.route("/jobs/:id/appy").put(isAuthenticatedUser,authorizeRoles("user"),applyJob)
 
-router.route("/jobs").post(isAuthenticatedUser,newjob)
+router.route("/jobs").post(isAuthenticatedUser,authorizeRoles("employeer"),newjob)
 router.route('/jobs/:zipcode/:distance').get(getJobsInRadius);
 router.route('/job/:id').put(isAuthenticatedUser ,updateJob)
     .delete(isAuthenticatedUser,deleteJob);
