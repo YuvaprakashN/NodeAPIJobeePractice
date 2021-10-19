2a5226ba34707a22367d2bf8073b03f8bc60fc6b
YuvaprakashN <73812241+YuvaprakashN@users.noreply.github.com>
2021-10-19 11:04:29 +0530

    apply job using resume

--- controller/jobsController.js
+++ controller/jobsController.js
@@ -1,12 +1,15 @@
+const geoCoder=require("../utils/geocoder")
+const path=require("path")
 const Job = require("../models/jobs")
 const ErrorHandler = require("../utils/errorHandler")
-const geoCoder=require("../utils/geocoder")
 const catchAsyncError=require("../middlewares/catchAsyncErrors")
 const APIFilters = require("../utils/apiFilters")
 exports.getJobs =catchAsyncError( async (req, res, err) => {
    
     const apiFilter=new APIFilters(Job.find(),req.query)
                         .filter()
+                        .sort()
+                        .limitFields()
     const jobs=await apiFilter.query
     res.status(200).json({
         success: true,
@@ -16,7 +19,10 @@ exports.getJobs =catchAsyncError( async (req, res, err) => {
 })
 
 exports.newjob =catchAsyncError( async (req, res, err) => {
+  
+
     console.log(`Adding New Job ${req.body}`);
+    req.body.user=req.user.id
     const job = await Job.create(req.body);
     res.status(200).json({
         success: true,
@@ -152,3 +158,68 @@ exports.jobStats =catchAsyncError( async (req, res, next) => {
         data: stats
     });
 });
+// Apply to job using Resume  =>  /api/v1/job/:id/apply
+exports.applyJob=catchAsyncError(async (req,res,next)=>{
+    let job = await Job.findById(req.params.id).select('+applicantsApplied');
+
+    if(!job){
+        return next(new ErrorHandler('Job not found.', 404));
+    }
+
+     // Check that if job last date has been passed or not
+     if (job.lastDate < new Date(Date.now())) {
+        return next(new ErrorHandler('You can not apply to this job. Date is over.', 400));
+    }
+
+    // Check if user has applied before
+    for (let i = 0; i < job.applicantsApplied.length; i++) {
+        if (job.applicantsApplied[i].id === req.user.id) {
+            return next(new ErrorHandler('You have already applied for this job.', 400))
+        }
+    }
+console.log(req);
+    // Check the files
+    if (!req.files) {
+        return next(new ErrorHandler('Please upload file.', 400));
+    }
+    const file = req.files.file;
+
+    // Check file type
+    const supportedFiles = /.docx|.pdf/;
+    if (!supportedFiles.test(path.extname(file.name))) {
+        return next(new ErrorHandler('Please upload document file.', 400))
+    }
+
+    // Check doucument size
+    if (file.size > process.env.MAX_FILE_SIZE) {
+        return next(new ErrorHandler('Please upload file less than 2MB.', 400));
+    }
+    // Renaming resume
+    file.name = `${req.user.name.replace(' ', '_')}_${job._id}${path.parse(file.name).ext}`;
+
+    file.mv(`${process.env.UPLOAD_PATH}/${file.name}`, async err => {
+        if (err) {
+            console.log(err);
+            return next(new ErrorHandler('Resume upload failed.', 500));
+        } 
+
+        await Job.findByIdAndUpdate(req.params.id, {
+            $push: {
+                applicantsApplied: {
+                    id: req.user.id,
+                    resume: file.name
+                }
+            }
+        }, {
+            new: true,
+            runValidators: true,
+        });
+
+        res.status(200).json({
+            success: true,
+            message: 'Applied to Job successfully.',
+            data: file.name
+        })
+
+    });
+})
